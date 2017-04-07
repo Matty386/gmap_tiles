be27eda7888f58c2b58f134c1437f9753ecb18dd
br382 <br382@drexel.edu>
2017-04-07 19:17:28 -0400

    Extended to use alternate map sources, threaded downloading, and other minor changes.

---
+++ main.py
@@ -0,0 +1,26 @@
+from sources        import *
+from download_tiles import *
+from merge_tiles    import *
+import webbrowser #just for opening the results of this test
+
+def main():
+    print 'Loading Region Information'
+    zoom = 15
+    lat_start, lon_start = 36.99, -114.03  #Grand Canyon USA (top left corner)
+    lat_stop, lon_stop   = 35.64, -111.60  #(bottom right corner)
+    coord = (lat_start, lat_stop, lon_start, lon_stop) #Will error if wrong corners specified
+    fname         = 'sources.json'
+    source_type   = {'type':'sat'}
+    found_sources = searchSource(fname, source_type)
+    key           = found_sources.keys()[0] #Just try first
+    source        = {key: found_sources[key]}
+    ext           = source[key]
+    print 'Downloading Tiles'
+    downloadTiles(source, zoom, coord, max_threads=1)
+    print 'Merging Tiles'
+    img_name = mergeTiles(source, zoom, coord)
+    print 'Done'
+    webbrowser.open(img_name)
+
+if __name__ == '__main__':
+    main()
---
+++ sources.py
@@ -0,0 +1,110 @@
+import json
+
+def savejson(filename, struct):
+    try:
+        for key in struct["sources"]:
+            tmp = struct["sources"][key]
+            newkey = str(hash(json.dumps(tmp)))
+            del struct["sources"][key]
+            struct["sources"][newkey] = tmp
+    except:
+        pass
+    with open(filename,'w') as fd:
+        fd.write( json.dumps(struct, indent=4, sort_keys=True) )
+
+def openjson(filename):
+    with open(filename, 'r') as fd:
+        t = fd.read()
+        s = json.loads(t)
+        if len(s) <= 0: return {}
+        return s
+
+def ppjson(inputs):
+    """
+    Pretty prints inputs which are assumed json filename, text, or structure.
+    """
+    try:
+        with open(inputs, 'r') as fd:
+            print json.dumps(json.loads(fd.read()), indent=4, sort_keys=True)
+    except:
+        try:
+            print json.dumps(json.loads(inputs), indent=4, sort_keys=True)
+        except:
+            print json.dumps(inputs, indent=4, sort_keys=True)
+
+def addSource(filename, type,name,prefix,postfix,x,y,zoom,ext='png', DEBUG=False):
+    try:
+        struct = openjson(filename)
+    except:
+        if DEBUG: print 'unable to save json contents'
+        return False
+    new = {}
+    new["type"]    = str(type)
+    new["name"]    = str(name)
+    new["prefix"]  = str(prefix)
+    new["postfix"] = str(postfix)
+    new["x"]       = str(x)
+    new["y"]       = str(y)
+    new["zoom"]    = str(zoom)
+    new["ext"]     = str(ext)
+    uid = str(hash( json.dumps(new) ))
+    try:
+        len(struct["sources"])
+    except:
+        struct["sources"] = {}
+    struct["sources"][uid] = new
+    try:
+        savejson(filename, struct)
+    except:
+        if DEBUG: print 'unable to save json contents'
+        return False
+    return True
+
+
+def searchSource(filename, search={}, DEBUG=False):
+    try:
+        struct = openjson(filename)
+    except:
+        struct = {"sources":{}}
+    output = {"sources":{}}
+    if DEBUG: print json.dumps(struct, indent=4, sort_keys=True)
+    if DEBUG: print search.keys()
+    for uid in struct["sources"].keys():
+        for attrib in search.keys():
+            try:
+                val = struct["sources"][uid][attrib]
+                test = (val.find( search[attrib] )>=0)
+                if DEBUG: print [uid,attrib,val,search[attrib], test]
+                if test:
+                    output["sources"][uid] = struct["sources"][uid]
+            except:
+                pass
+    return output["sources"]
+
+
+def rmSource(filename, uid):
+    try:
+        struct = openjson(filename)
+        del struct["sources"][uid]
+        savejson(filename, struct)
+    except:
+        return False
+    return True
+
+
+def main():
+    print '-- Insert Test Source'
+    fname='sources.json'
+    addSource(fname, 'Satellite','Test Source','www.google.com/','&fetch=True','&x=','&y=','&z=')
+    ppjson(fname)
+    print '-- Search For Test Source'
+    found = searchSource(fname, search={"name":"ource"})
+    ppjson(found)
+    print '-- Remove Test Source'
+    for key in found.keys():
+        print '--- Removing: ', key, rmSource(fname, key)
+    ppjson(fname)
+
+if __name__ == '__main__':
+    main()
+
